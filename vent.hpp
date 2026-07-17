b3b887454c9edcf73a86a499442e935fe994bb72
hughes6 <53721538+hughes6@users.noreply.github.com>
2026-07-16 18:06:52 -0600

    Refactor Vent class to use size array instead of width/height

--- vent.hpp
+++ vent.hpp
@@ -12,33 +12,31 @@ struct Vent {
     double height_m;
     double free_area_ratio; // 0.0 to 1.0
 
+    std::array<double, 3> size;      // m
     std::array<double, 3> center;    // m
     std::array<double, 3> direction; // outward/inward normal direction
 
     Vent() : name("Uninitialized"), 
-             width_m(0.0),
-             height_m(0.0),
+             size{0.0, 0.0, 0.0},
              free_area_ratio(0.0),
              center{0.0, 0.0, 0.0},
              direction{0.0, 0.0, 0.0} {}
 
     Vent(std::string name_,
-         double width_m_,
-         double height_m_,
+         std::array<double, 3> size_,
          double free_area_ratio_,
          std::array<double, 3> center_,
          std::array<double, 3> direction_) :
          name(name_),
-         width_m(width_m_),
-         height_m(height_m_),
-         free_area_ratio(free_area_ratio_),
+         size(size_),
          center(center_),
-         direction(direction_) {}
+         direction(direction_) {set_free_area_ratio(free_area_ratio_);}
 
     // setters
     void set_name(std::string n) { name = n; }
-    void set_width_m(double w) { width_m = w; }
-    void set_height_m(double h) { height_m = h; }
+    void set_size(std::array<double, 3> s) {
+        size = s;
+    }
     void set_free_area_ratio(double a) {
         if(a >= 0.0 && a <= 1.0) { free_area_ratio = a; }
         else {
@@ -48,8 +46,7 @@ struct Vent {
 
     //getters
     std::string get_name() const { return name; }
-    double get_width_m() const { return width_m; }
-    double get_height_m() const { return height_m; }
+    std::array<double, 3> get_size() const { return size; }
     double get_free_area_ratio() const { return free_area_ratio; }
     std::array<double, 3> get_center() const { return center; }
     std::array<double, 3> get_direction() const { return direction; }
