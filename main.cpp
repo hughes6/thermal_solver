bd283fea5f96499f40ac01658fa896a32d7d9086
hughes6 <53721538+hughes6@users.noreply.github.com>
2026-07-16 18:04:23 -0600

    Refactor main.cpp with environment and flow solver
    Updated main.cpp to include environment and flow solver components, added humidity and elevation parameters, and modified fan and vent configurations.

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@
 
 #include "cell.hpp"
 #include "component.hpp"
+#include "environment.hpp"
 #include "fan.hpp"
+#include "flow_solver.hpp"
 #include "grapher.hpp"
 #include "mesh.hpp"
 #include "rack.hpp"
 #include "solver.hpp"
+#include "unit_test.hpp"
 #include "vent.hpp"
 
 /* 
 =========================================================
 TODO
 ---------------------------------------------------------
- - add rack walls with vents
  - adaptive meshing
  - make sure temps lower to rt right? Newtonian
  - add specialized components, cpu location & fan
@@ -46,76 +48,121 @@ TODO
 */
 
 int main(int argc, char* argv[]) {
-    double dx = 0.04445;   // 1U
-    double dy = 0.04445;   // 1U
-    double dz = 0.04445/2.0;   // 1U
-    double sim_length = 20; 
-    double dt = 0.1;
+  // ==================================
+  // UNIT TEST
+  // ==================================
+  // UnitTest tester;
+  // tester.run();
 
-    Rack rack = Rack::from_rack_units(
-       10.0,      // height (U)
-       8.75,     // width (5U = 5 × 1.75 in)
-       8.75      // depth (5U = 5 × 1.75 in)
-    );  
+  
+  double dx = 0.04445;   // 1U
+  double dy = 0.04445;   // 1U
+  double dz = 0.04445/2.0;   // 1U
+  double sim_length = 2; 
+  double dt = 0.1;
+  double mu = 1.8e-5;  // air at atmospheric pressure and temp
+  double pr = 0.71;    // air at atmospheric pressure and temp
 
-    rack.set_t(20);
-    rack.set_cp(1000);
-    rack.set_h(1);
-    rack.set_k(1);
-    rack.set_rho(1);
-    Fan top_fan{
-      "Top exhust", // name
-      3.0,        // cfm
-      0.1,          // diameter (m)
-      {rack.get_width_m()/2, rack.get_depth_m()/2, rack.get_height_m()}, // m m m
-      {0.0, 0.0, 1.0}, // velocity vector 0x 0y 1z air going up
-      FlowType::Exhaust
-    };
+  double humidity_pct = 30.0;
+  double elevation_ft = 6000.0;
+  Environment env(humidity_pct, elevation_ft);  
 
-    Vent vent("Rack main vent", 0.1, 0.1, 0.5,
-       {rack.get_width_m()/2, 0.0, rack.get_height_m()*0.25},
-      {})
+  Rack rack = Rack::from_rack_units(
+      10.0,     // height (U)
+      8.75,     // width (5U = 5 × 1.75 in)
+      8.75      // depth (5U = 5 × 1.75 in)
+  );  
 
-    Mesh mesh = Mesh().build_mesh(rack, dx, dy, dz);
-    // mesh.print_mesh();
+  rack.set_t(20);        // 68 F
+  rack.set_cp(1005);     // cp of air
+  rack.set_k(0.02587);   // k of air at 20 C
+  rack.set_rho(1.225);   // density of air kg/m^3
 
-    Component server1 = Component::from_rack_units(1.0, 8.75, 8.75, "S-1"); // u in in
-    server1.set_coords_rack_units(0,0,1); // in in u
-    server1.set_k_solid(200.0);    // W/(m·K)
-    server1.set_cp(900.0);         // J/(kg·K)
-    server1.set_rho_solid(2700.0); // kg/m^3
-    server1.set_t(20.0);           // °C
-    server1.set_watts(1000);      // W
+  Fan top_fan{
+    "Top exhust", // name
+    3.0,        // cfm
+    0.1,          // diameter (m),
+    {0.0, 0.0, 0.0}, // rectangular size
+    {rack.get_width_m()/2, rack.get_depth_m()/2, rack.get_height_m()}, // m m m
+    {0.0, 0.0, 1.0}, // velocity vector 0x 0y 1z air going up
+    FlowType::Exhaust,
+    ShapeType::Circular
+  };
 
-   //  Component server2 = Component::from_rack_units(1.0, 8.75, 8.75, "S-2"); // u in in
-   //  server2.set_coords_rack_units(0,0,2); // in in u
-   //  server2.set_k_solid(100.0);    // W/(m·K)
-   //  server2.set_cp(400.0);         // J/(kg·K)
-   //  server2.set_rho_solid(2100.0); // kg/m^3
-   //  server2.set_t(90.0);           // °C
-   //  server2.set_watts(10000);      // W
+  Fan mid_fan{
+    "mid intake", // name
+    3.0,        // cfm
+    0.0,          // diameter (m),
+    {0.0, 0.1, 0.1}, // rectangular size
+    {rack.get_width_m(), rack.get_depth_m()/2, rack.get_height_m() * 0.5}, // m m m center
+    {-1.0, 0.0, 0.0}, // velocity vector 0x 0y 0z air going in
+    FlowType::Intake,
+    ShapeType::Rectangular
+  };
 
-    Grapher grapher(rack, dx, dy, dz);
-    grapher.add_component(server1);
-   //  grapher.add_component(server2);
-    grapher.add_fan(top_fan);
-    grapher.stamp_components();
-    grapher.stamp_fans();
-   //  grapher.print_bitmap();
-    grapher.export_to_file("output.txt");
+  Vent vent("Rack main vent", 
+      {0.1, 0.0, 0.1},
+      0.5,
+      {rack.get_width_m()/2, 0.0, rack.get_height_m()*0.5},
+      {0.0, 1.0, 0.0}
+  );
 
-    mesh.stamp_component(server1);
-    mesh.stamp_fan(top_fan);
-   //  mesh.stamp_component(server2);
-   //  mesh.print_mesh();
+  Mesh mesh = Mesh().build_mesh(rack, dx, dy, dz, mu, pr);
+  // mesh.print_mesh();
 
-    Solver solver(mesh, dt, sim_length);
-   //  solver.apply_bulk_velocity(0.0, 0.0, 0.0222);
-    /*advection stability ratio C = vz * dt / dz
-      should  be C <= 1
-      so C = 0.0222*0.1/0.0222 = 1*/
-   
-    solver.solve();
-    // Mesh final_mesh = solver.get_mesh();
-    return 0;
+  Component server1 = Component::from_rack_units(1.0, 8.75, 8.75, "S-1"); // u in in
+  server1.set_coords_rack_units(0,0,1); // in in u
+  server1.set_k_solid(200.0);    // W/(m·K)
+  server1.set_cp(900.0);         // J/(kg·K)
+  server1.set_rho_solid(2700.0); // kg/m^3
+  server1.set_t(20.0);           // °C
+  server1.set_watts(1000);      // W
+
+  //  Component server2 = Component::from_rack_units(1.0, 8.75, 8.75, "S-2"); // u in in
+  //  server2.set_coords_rack_units(0,0,2); // in in u
+  //  server2.set_k_solid(100.0);    // W/(m·K)
+  //  server2.set_cp(400.0);         // J/(kg·K)
+  //  server2.set_rho_solid(2100.0); // kg/m^3
+  //  server2.set_t(90.0);           // °C
+  //  server2.set_watts(10000);      // W
+
+  Grapher grapher(rack, dx, dy, dz);
+  grapher.add_component(server1);
+  //  grapher.add_component(server2);
+  grapher.add_fan(top_fan);
+  grapher.add_fan(mid_fan);
+  grapher.add_vent(vent);
+  grapher.stamp_components();
+  grapher.stamp_fans();
+  grapher.stamp_vents();
+  // grapher.print_bitmap();
+  grapher.export_to_file("output.txt");
+
+  double vent_discharge_coeff = 0.5;
+  mesh.stamp_component(server1);
+  mesh.stamp_fan(top_fan);
+  mesh.stamp_fan(mid_fan);
+  double Cd = 0.5;
+  mesh.stamp_vent(vent, vent_discharge_coeff);
+  //  mesh.stamp_component(server2);
+  // mesh.print_mesh();
+  
+  double resistivity = 4.5;
+  double tolerance = 1e-6;
+  int max_iterations = 1000;
+  double sor_omega = 1.2;
+
+  FlowSolver flow_solver(mesh, resistivity, vent_discharge_coeff, tolerance, max_iterations, sor_omega);
+  flow_solver.solve(); // pre populate all velocity cells
+
+  Solver solver(mesh, dt, sim_length);
+  //  solver.apply_bulk_velocity(0.0, 0.0, 0.0222);
+  /*advection stability ratio C = vz * dt / dz
+    should  be C <= 1
+    so C = 0.0222*0.1/0.0222 = 1*/
+
+  solver.solve();
+  // Mesh final_mesh = solver.get_mesh();
+  // final_mesh.print_mesh_layer_temp(3);
+  return 0;
 }
