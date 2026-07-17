5ff96af624bc8a9bb91d609645c8a0e4a1a72ba6
hughes6 <53721538+hughes6@users.noreply.github.com>
2026-07-16 18:03:17 -0600

    Add ShapeType enum and update Fan struct

--- fan.hpp
+++ fan.hpp
@@ -11,6 +11,12 @@ enum class FlowType {
     Uninitialized
 };
 
+enum class ShapeType {
+    Circular,
+    Rectangular,
+    Uninitialized
+};
+
 struct Fan {
     static constexpr double CFM_TO_M3S = 0.00047194745;
     static constexpr double PI = 3.14159265358979323846;    
@@ -25,7 +31,10 @@ struct Fan {
     // Example top exhaust: {0,0,1}
     // Front intake: {0,1,0} or {1,0,0} depending on your axes
     std::array<double, 3> direction{0.0, 0.0, 1.0};
+    std::array<double, 3> size{0.0, 0.0, 0.0};
+
     FlowType type = FlowType::Uninitialized;
+    ShapeType shape = ShapeType::Uninitialized;
 
     Fan() : name("Uninitialized"),
             cfm(0.0),
@@ -36,15 +45,31 @@ struct Fan {
     Fan(std::string name_,
         double cfm_,
         double diameter_m_,
+        std::array<double, 3> size_,
         std::array<double, 3> center_,
         std::array<double, 3> direction_,
-        FlowType type_ = FlowType::Uninitialized)
+        FlowType type_ = FlowType::Uninitialized,
+        ShapeType shape_ = ShapeType::Uninitialized)
         : name(std::move(name_)),
           cfm(cfm_),
           diameter_m(diameter_m_),
+          size(size_),
           center(center_),
           direction(direction_),
-          type(type_) {}
+          type(type_), 
+          shape(shape_) { 
+            check_shape_dimensions();
+        }
+
+
+    void check_shape_dimensions() {
+        if(shape == ShapeType::Circular && (size[0] != 0.0 || size[1] != 0.0 || size[2] != 0.0)) {
+            std::cerr << "Invalid size, cartesian size defined for radial fan \n";
+        }
+        if(shape == ShapeType::Rectangular && diameter_m != 0.0) {
+            std::cerr << "Invalid size, diameter defined for a rectangular fan \n";
+        }
+    }   
 
     double velocity_x() const { return velocity_mag() * direction[0]; }
     double velocity_y() const { return velocity_mag() * direction[1]; }
@@ -54,11 +79,15 @@ struct Fan {
     double velocity_mag() const { return area() > 0.0 ? flow_m3s() / area() : 0.0; }
     std::array<double, 3> get_center() const { return center; }
     std::array<double, 3> get_velocity_dir() const { return direction;} 
+    std::array<double, 3> get_size() const { return size; }
     std::string get_name() const { return name; }
     double get_cfm() const { return cfm; }
     double get_diameter() const { return diameter_m; }
     std::string get_type() const { return (type == FlowType::Exhaust) ? "Exhaust" : "Intake"; }
+    std::string get_shape() const { return (shape == ShapeType::Circular) ? "Circular" : "Rectangular"; }
     FlowType get_type_t() const { return type; }
+    ShapeType get_shape_t() const { return shape; }
+    bool is_circular() const { return shape == ShapeType::Circular; }
 };
 
 #endif
